41440275a47d30afeac73a473df1c4febbb27891
Suraj Shekhar <surajshekhar129@gmail.com>
2025-03-25 19:18:42 +0530

    removed unnecessary imports

--- lib/config/app_routes.dart
+++ lib/config/app_routes.dart
@@ -1,7 +1,6 @@
 import 'package:flutter/material.dart';
 import '../presentation/screens/catalogue_screen.dart';
 import '../presentation/screens/cart_screen.dart';
-import '../presentation/screens/cart_screen.dart';
 
 class AppRoutes {
   static const String catalogue = '/';
@@ -10,11 +9,11 @@ class AppRoutes {
   static Route<dynamic> generateRoute(RouteSettings settings) {
     switch (settings.name) {
       case catalogue:
-        return MaterialPageRoute(builder: (_) => CatalogueScreen());
+        return MaterialPageRoute(builder: (_) => const CatalogueScreen());
       case cart:
-        return MaterialPageRoute(builder: (_) => CartScreen());
+        return MaterialPageRoute(builder: (_) => const CartScreen());
       default:
-        return MaterialPageRoute(builder: (_) => CatalogueScreen());
+        return MaterialPageRoute(builder: (_) => const CatalogueScreen());
     }
   }
 }
